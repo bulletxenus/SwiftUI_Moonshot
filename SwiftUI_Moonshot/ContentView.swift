f5df9a22a40a4a5eb7ce2c5c107033e0f3752fd7
Dzmitry Khamitsevich <dzmitry_khamitsevich@epam.com>
2023-12-24 16:49:33 +0200

    Initial Commit

---
+++ SwiftUI_Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Moonshot
+//
+//  Created by Dzmitry Khamitsevich on 24.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Moonshot/SwiftUI_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MoonshotApp.swift
+//  SwiftUI_Moonshot
+//
+//  Created by Dzmitry Khamitsevich on 24.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
